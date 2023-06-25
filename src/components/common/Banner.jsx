8ba49e6a943f4f9405b6dae6bb53fa43f8e6172e
leeskies <lee2dskies@gmail.com>
2023-06-25 19:20:11 +0300

    yo

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -5,9 +5,9 @@ export const Banner = () => {
   return (
     <div className='absolute inset-0 bg-gradient-to-br from-zinc-900 to-slate-900 flex items-center justify-center'>
         <article className='flex space-x-8 border-black items-center'>
-            <TST w={80} />
+            {/* <TST w={80} /> */}
             <div className='bg-gradient-to-r from-sky-600 to-slate-200 bg-clip-text'>
-                <h1 className='text-[200px] text-transparent font-bold'>SIPPETS</h1>
+                <h1 className='xl:text-[200px] md:text-[150px] text-transparent font-bold'>SIPPETS</h1>
             </div>
         </article>
     </div>
--- src/components/protected/Discover/DiscoverItem.jsx
+++ src/components/protected/Discover/DiscoverItem.jsx
@@ -8,10 +8,14 @@ export const DiscoverItem = ({ user }) => {
   return (
     <button onClick={() => navigate('/user/' + user._id)} className='flex items-center w-full space-x-4 text-slate-800 bg-stone-300 rounded p-1'>
         <article>
-            <img src={user.image.replace('upload/', 'upload/c_fill,h_200,w_200/')}
+            {user?.image? <img src={user.image.replace('upload/', 'upload/c_fill,h_200,w_200/')}
             alt={<p className="font-bold ">
                   {user.username.charAt(0).toUpperCase()}
                 </p>} className='w-10 rounded aspect-square' />
+                :
+                <p className="font-bold ">
+                  {user.username.charAt(0).toUpperCase()}
+                </p>}
         </article>
         <p className='text-xl'>{user.username}</p>
     </button>
