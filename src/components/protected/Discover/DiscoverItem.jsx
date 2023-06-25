7ed646e7bbcc2da251abfe205120aa7838374f10
leeskies <lee2dskies@gmail.com>
2023-06-25 19:39:29 +0300

    yo

--- src/components/protected/Discover/DiscoverItem.jsx
+++ src/components/protected/Discover/DiscoverItem.jsx
@@ -5,15 +5,37 @@ export const DiscoverItem = ({ user }) => {
 
     const navigate = useNavigate()
 
+    const getRandomColor = () => {
+      const midRangeColors = [
+        '#89CFF0',
+        '#0000FF',
+        '#7393B3',
+        '#088F8F',
+        '#0096FF',
+        '#0047AB',
+        '#6495ED',
+        '#1434A4',
+        '#1F51FF',
+        '#4169E1',
+        '#4682B4',
+      ];
+    
+      const randomIndex = Math.floor(Math.random() * midRangeColors.length);
+      return midRangeColors[randomIndex];
+    };
+
+    const randomColor = getRandomColor()
+
   return (
     <button onClick={() => navigate('/user/' + user._id)} className='flex items-center w-full space-x-4 text-slate-800 bg-stone-300 rounded p-1'>
         <article>
             {user?.image? <img src={user.image.replace('upload/', 'upload/c_fill,h_200,w_200/')}
-            alt={<p className="font-bold w-10 h-10 text-center flex items-center justify-center">
+            alt={<p
+            style={{backgroundColor: randomColor}} className="font-bold w-10 h-10 text-center flex items-center justify-center">
                   {user.username.charAt(0).toUpperCase()}
                 </p>} className='w-10 rounded aspect-square' />
                 :
-                <p className="font-bold w-10 h-10 text-center flex items-center justify-center">
+                <p style={{backgroundColor: randomColor}} className="font-bold w-10 h-10 text-center flex items-center justify-center">
                   {user.username.charAt(0).toUpperCase()}
                 </p>}
         </article>
