e44dd6c4cefce143fd4ce369c8436fa1dbd34c9d
leeskies <lee2dskies@gmail.com>
2023-06-25 22:35:59 +0300

    yo

--- src/components/common/Profile/ProfileNav.jsx
+++ src/components/common/Profile/ProfileNav.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const ProfileNav = ({ _id, id, display, setDisplay, changeDisplay }) => {
   return (
     <nav className={`relative w-full border-t border-t-slate-600 flex [&_button]:text-center items-center [&_button]:p-4 [&_button]:grow bg-slate-700
-    after:w-1/2 after:h-1 after:bg-slate-900 after:hidden after:bottom-0 after:left-0 ${display == 'sippets' ? '' : 'after:translate-x-[100%]'} after:duration-300`}>
+    after:w-1/2 after:h-1 after after:bg-slate-900 after:absolute after:bottom-0 after:left-0 ${display == 'sippets' ? '' : 'after:translate-x-[100%]'} after:duration-300`}>
       <button onClick={() => changeDisplay('sippets')}>
         Sippets
       </button>
