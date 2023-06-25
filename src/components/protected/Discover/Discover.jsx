d1030f0edde0a238c565b67284a36b98ed51eb4d
leeskies <lee2dskies@gmail.com>
2023-06-25 22:09:27 +0300

    yo

--- src/components/protected/Discover/Discover.jsx
+++ src/components/protected/Discover/Discover.jsx
@@ -16,14 +16,14 @@ export const Discover = () => {
   }, [])
   return (
     <div className='w-full py-4'>
-      <p className='font-extralight italic text-lg pb-1 pl-2'>You might be interested in:</p>
+      {/* <p className='font-extralight italic text-lg pb-1 pl-2'>You might be interested in:</p>
       <section className='border rounded border-slate-600 p-2 space-y-4'>
         {users.map((user, i) => (
           <div key={i}>
             <DiscoverItem user={user} />
           </div>
         ))}
-      </section>
+      </section> */}
     </div>
   )
 }
