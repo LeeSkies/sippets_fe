45530e446129de7e823748fa8f3139858d5d6ee3
leeskies <lee2dskies@gmail.com>
2023-06-25 21:52:47 +0300

    yo

--- src/context/websocketContext.jsx
+++ src/context/websocketContext.jsx
@@ -1,53 +1,53 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import io from 'socket.io-client'
-import { UserContext } from './userContext'
-import instance from "../services/axios";
+// import { createContext, useContext, useEffect, useState } from "react";
+// import io from 'socket.io-client'
+// import { UserContext } from './userContext'
+// import instance from "../services/axios";
 
-export const WebsocketContext = createContext({
-    socket: null,
-    auth: null,
-    conversations: [],
-})
+// export const WebsocketContext = createContext({
+//     socket: null,
+//     auth: null,
+//     conversations: [],
+// })
 
-export const WebsocketProvider = ({ children }) => {
-    const [socket, setSocket] = useState()
-    const [auth, setAuth] = useState(() => {
-        let result = '';
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        for (let i = 0; i < 150; i++) {
-          result += characters.charAt(Math.floor(Math.random() * characters.length));
-        }
-        return result;
-      })
-    const [conversations, setConversations] = useState([])
-    const [currentReceiver, setCurrentReceiver] = useState(null)
+// export const WebsocketProvider = ({ children }) => {
+//     const [socket, setSocket] = useState()
+//     const [auth, setAuth] = useState(() => {
+//         let result = '';
+//         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//         for (let i = 0; i < 150; i++) {
+//           result += characters.charAt(Math.floor(Math.random() * characters.length));
+//         }
+//         return result;
+//       })
+//     const [conversations, setConversations] = useState([])
+//     const [currentReceiver, setCurrentReceiver] = useState(null)
 
-    const { user } = useContext(UserContext)
+//     const { user } = useContext(UserContext)
 
-    const fetchConversations = async () => {
-        const { data } = await instance.get('/protected/conversation/all', { withCredentials: true })
-        setConversations(data)
-    }
+//     const fetchConversations = async () => {
+//         const { data } = await instance.get('/protected/conversation/all', { withCredentials: true })
+//         setConversations(data)
+//     }
 
-    useEffect(() => {
-        let connection;
-        if (user?._id) {
-            connection = io(, { query: { userId: user._id, handshakeString: auth } });
-            connection.connect();
-            setSocket(connection);
-            fetchConversations()
-        }
-        return () => {
-            if (connection) {
-                connection.close();
-            }
-        };
-    }, [user]);
+//     useEffect(() => {
+//         let connection;
+//         if (user?._id) {
+//             connection = io(, { query: { userId: user._id, handshakeString: auth } });
+//             connection.connect();
+//             setSocket(connection);
+//             fetchConversations()
+//         }
+//         return () => {
+//             if (connection) {
+//                 connection.close();
+//             }
+//         };
+//     }, [user]);
     
 
-    return (
-        <WebsocketContext.Provider value={{ socket, auth, conversations, currentReceiver, setCurrentReceiver }}>
-            {children}
-        </WebsocketContext.Provider>
-    )
-}
+//     return (
+//         <WebsocketContext.Provider value={{ socket, auth, conversations, currentReceiver, setCurrentReceiver }}>
+//             {children}
+//         </WebsocketContext.Provider>
+//     )
+// }
