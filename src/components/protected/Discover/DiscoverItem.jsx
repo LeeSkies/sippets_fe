33323388d860865887ef31ffd75da466c20b1016
leeskies <lee2dskies@gmail.com>
2023-06-25 19:25:29 +0300

    yo

--- src/components/protected/Discover/DiscoverItem.jsx
+++ src/components/protected/Discover/DiscoverItem.jsx
@@ -9,11 +9,11 @@ export const DiscoverItem = ({ user }) => {
     <button onClick={() => navigate('/user/' + user._id)} className='flex items-center w-full space-x-4 text-slate-800 bg-stone-300 rounded p-1'>
         <article>
             {user?.image? <img src={user.image.replace('upload/', 'upload/c_fill,h_200,w_200/')}
-            alt={<p className="font-bold w-10 h-10">
+            alt={<p className="font-bold w-10 h-10 text-center flex items-center justify-center">
                   {user.username.charAt(0).toUpperCase()}
                 </p>} className='w-10 rounded aspect-square' />
                 :
-                <p className="font-bold w-10 h-10">
+                <p className="font-bold w-10 h-10 text-center flex items-center justify-center">
                   {user.username.charAt(0).toUpperCase()}
                 </p>}
         </article>
