0afe5692f6a9a47596bf8e660ed9fa41ca678366
leeskies <lee2dskies@gmail.com>
2023-06-26 17:34:49 +0300

    yo

--- src/components/common/ImageComp.jsx
+++ src/components/common/ImageComp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const ImageComp = ({ url, w = '100%', h = '100%' }) => {
   return (
     <figure style={{ width: w, height: h }}
-    className='rounded-md flex justify-center items-center overflow-clip'>
+    className='flex justify-center items-center overflow-clip'>
       <img src={url} className='min-w-full min-h-full object-cover' />
     </figure>
   )
