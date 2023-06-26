a715d738d8fbd7b0b38792b6395b7ed5ab109d9c
leeskies <lee2dskies@gmail.com>
2023-06-26 13:03:51 +0300

    yo

--- src/components/sippets/SippetDisplay.jsx
+++ src/components/sippets/SippetDisplay.jsx
@@ -66,9 +66,9 @@ export const SippetDisplay = ({ sippet }) => {
           {/* {console.log(sippet)} */}
           </article>
           {sippet.file &&
-          <div className='rounded-xl aspect-square m-3 md:m-6 overflow-clip mt-3'>
+          <div className='rounded-xl max-h-[500px] m-3 md:m-6 overflow-clip mt-3'>
             <img loading='lazy' onClick={(e) => {e.stopPropagation(), setModal(prev => !prev)}}
-            src={imgURL} className='w-full' />
+            src={'https://res.cloudinary.com/db7vikkkf/image/upload/v1687646202/oal9cbly9shwc0lalant.jpg'} className='w-full' />
           </div>}
           {console.log(sippet.is == 'toast' && sippet.ref_sippet.file.secure_url)}
           {modal && <ImageModal cb={setModal} url={imgURL} />}
