048af875ed103138f68c48b0d735d8693daf9437
leeskies <lee2dskies@gmail.com>
2023-06-26 21:06:56 +0300

    yo

--- src/components/protected/ProtectedProfile.jsx
+++ src/components/protected/ProtectedProfile.jsx
@@ -107,7 +107,7 @@ export const ProtectedProfile = () => {
 
   return (
     loading ? <SippetSkeleton icon={true} count={7} />
-    : <div className="relative bg-no-repeat bg-cover flex flex-col items-center w-full min-h-screen [&_*]:-10 pb-4" >
+    : user && <div className="relative bg-no-repeat bg-cover flex flex-col items-center w-full min-h-screen [&_*]:-10 pb-4" >
       <div className='absolute -z-10 w-full left-0 right-0 top-0 h-[90vh] bg-gradient-to-b from-[#1e1e1f] to-transparent pb-20'>
       </div>
       <header className='self-start'>
