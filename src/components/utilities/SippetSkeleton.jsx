828dab90577273ffa04644478e153adde161a862
leeskies <lee2dskies@gmail.com>
2023-06-27 00:49:19 +0300

    yo

--- src/components/utilities/SippetSkeleton.jsx
+++ src/components/utilities/SippetSkeleton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const SippetSkeleton = ({ count = 4, w, h, icon }) => {
   return (
     <div className="w-full flex flex-col [&_*]:bg-opacity-70 rounded p-4 space-y-4 [&_*]:animate-pulse">
-        {icon && <div className="h-16 w-16 bg-gray-300 self-end rounded-md "></div>}
+        {icon && <div className="h-16 w-16 bg-gray-300 self-end rounded-md flex flex-col justify-start"></div>}
         {new Array(count).fill(null).map((row, i) => (
           <div key={i} style={{
             height: `${h ? h : '1rem'}`,
