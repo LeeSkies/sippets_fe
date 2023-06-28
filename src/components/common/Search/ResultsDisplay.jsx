da4b8104096c44655b89cad4740c2ef37ef6843e
leeskies <lee2dskies@gmail.com>
2023-06-29 00:33:26 +0300

    x

--- src/components/common/Search/ResultsDisplay.jsx
+++ src/components/common/Search/ResultsDisplay.jsx
@@ -5,7 +5,7 @@ import { UserResult } from './UserResult'
 export const ResultsDisplay = ({ param, results }) => {
   return (
     <section className='absolute bot-0 w-full z-20 bg-gradient-to-r from-sky-200 shadow to-neutral-600 text-slate-600'>
-        {results.map((result, i) => param == 'text' ? <TextResult result={result} /> : <UserResult />)}
+        {results.map((result, i) => param == 'text' ? <TextResult  <UserResult />)}
     </section>
   )
 }
