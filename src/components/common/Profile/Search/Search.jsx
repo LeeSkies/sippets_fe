68269070738a670114211a5a8c623c8222be16b1
leeskies <lee2dskies@gmail.com>
2023-06-28 15:22:14 +0300

    y

--- src/components/common/Profile/Search/Search.jsx
+++ src/components/common/Profile/Search/Search.jsx
@@ -5,7 +5,7 @@ export const Search = () => {
     const [results, setResults] = useState([])
     const [loading, setLoading] = useState(true)
   return (
-    <div className='w-full py-2'>
+    <div className='w-full py-2 border-t border-t-slate-600'>
         <SearchBar />
     </div>
   )
