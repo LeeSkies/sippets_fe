d485dd18627c15898e3f03adc548199e64198739
leeskies <lee2dskies@gmail.com>
2023-06-28 22:17:53 +0300

    x

--- src/components/common/Profile/Search/ResultsDisplay.jsx
+++ src/components/common/Profile/Search/ResultsDisplay.jsx
@@ -3,7 +3,7 @@ import { TextResult } from './TextResult'
 
 export const ResultsDisplay = ({ results }) => {
   return (
-    <section className='absolute bot-0 w-full h-24 z-20 bg-slate-100'>
+    <section className='absolute bot-0 w-full z-20 bg-slate-100 text-slate-900'>
         {results.map((result, i) => <TextResult result={result} />)}
     </section>
   )
