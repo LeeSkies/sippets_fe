fcaa79a5eeb48339270192ad6f4a272a02642502
leeskies <lee2dskies@gmail.com>
2023-06-28 12:17:09 +0300

    yo

--- src/components/protected/SideMenu.jsx
+++ src/components/protected/SideMenu.jsx
@@ -8,7 +8,7 @@ import { NavLink } from 'react-router-dom'
 export const SideMenu = ({ }) => {
     
     const { notifications, setNotifications } = useContext(NotificationsContext)
-    const { user } = useContext(UserContext)
+    const { user, logout } = useContext(UserContext)
 
     const [open, setOpen] = useState(false)
 
@@ -44,7 +44,7 @@ export const SideMenu = ({ }) => {
                 {links.map((link, i) => (
                     <NavLink to={link.to} className='p-2'>{link.icon}</NavLink>
                 ))}
-                <button className='text-red-700 p-2 pb-1'>
+                <button onClick={logout} className='text-red-700 p-2 pb-1'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
                       <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
                       <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z"/>
