885ffebcf8c9f6a39df8a38836b48b4ae7dd23a1
leeskies <lee2dskies@gmail.com>
2023-06-28 15:38:46 +0300

    y

--- src/components/common/Profile/Search/SearchBar.jsx
+++ src/components/common/Profile/Search/SearchBar.jsx
@@ -5,7 +5,7 @@ export const SearchBar = () => {
     const inputRef = useRef()
   return (
     <section className='w-full flex items-center overflow-clip bg-gradient-to-r from-sky-200 to-transparent'>
-        <input ref={inputRef} autoFocus={true} type="text" className='p-2 grow bg-transparent focus:outline-none' />
+        <input ref={inputRef} autoFocus={true} type="text" className='p-2 grow bg-transparent focus:outline-none caret-neutral-900 text-neutral-900' />
         <button className=''>
             <MagnifyingGlassIcon className='w-6 h-6 text-sky-400 pr-2' />
         </button>
