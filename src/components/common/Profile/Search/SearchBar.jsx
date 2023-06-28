aa441af1071ab50dca5e7f7aae1645bf32c9bde9
leeskies <lee2dskies@gmail.com>
2023-06-28 17:45:03 +0300

    x

--- src/components/common/Profile/Search/SearchBar.jsx
+++ src/components/common/Profile/Search/SearchBar.jsx
@@ -10,7 +10,7 @@ export const SearchBar = () => {
     }
   return (
     <section className='w-full flex items-center overflow-clip bg-gradient-to-r from-sky-200 to-transparent'>
-        <button className='w-[100px] rounded-full p-1 bg-sky-300 flex text-slate-700 justify-center items-center'>
+        <button className='w-[100px] rounded-full p-1 space-x-1 bg-sky-300 flex text-slate-700 justify-center items-center'>
             <p>{param}</p>
             <ArrowPathRoundedSquareIcon className='w-3 h-3 text-slate-300' />
         </button>
