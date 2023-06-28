a7f5c352d877b5689a919665fb1a2c02d410463c
leeskies <lee2dskies@gmail.com>
2023-06-28 17:59:20 +0300

    x

--- src/components/common/Profile/Search/SearchBar.jsx
+++ src/components/common/Profile/Search/SearchBar.jsx
@@ -11,7 +11,7 @@ export const SearchBar = () => {
   return (
     <section className='w-full flex items-center overflow-clip bg-gradient-to-r from-sky-200 to-transparent pl-1'>
         <button onClick={handleChange} className='w-[100px] rounded-full p-[2px] px-2 space-x-1 border bg-slate-700 flex text-slate-300 items-center'>
-            <p className='pb-1 grow'>{param}</p>
+            <p className='pb-[2px] grow'>{param}</p>
             <ArrowPathRoundedSquareIcon className='w-[12px] h-[12px]' />
         </button>
         <input ref={inputRef} autoFocus={true} type="text" className='p-2 grow bg-transparent focus:outline-none caret-neutral-900 text-neutral-900' />
