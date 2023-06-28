c29d69c60d30e04ac75bbc66665de92684aa8eac
leeskies <lee2dskies@gmail.com>
2023-06-28 23:38:39 +0300

    x

--- src/components/common/Profile/Search/TextResult.jsx
+++ src/components/common/Profile/Search/TextResult.jsx
@@ -24,12 +24,12 @@ export const TextResult = ({ result }) => {
             : <p className='rounded w-[40px] flex items-center justify-center h-[40px] bg-blue-600'>{result.author.username.charAt(0)}</p>}
             <p>{result.author.username}</p>
         </header>
-        <p className='font-bold text-sm w-full'>{buildStr(70)}...</p>
+        <p className='font-bold text-sm w-full text-left'>{buildStr(70)}...</p>
         <footer className='w-full flex justify-between'>
             <p>{result.blocks.find(b => b.type === 'code') ? result.language + ' code' : 'no code'}</p>
-            <p className='flex items-center space-x-1'>
+            <p className='flex items-center space-x-1 text-rose-800'>
                 {result.likesCount}
-                <HeartIcon className='w-5 h-5' />
+                <HeartIcon className='w-5 h-5 animate-heartbeat' />
             </p>
         </footer>
     </button>
