f73175b3acc698f46269dc5976a606d3eda1b6e7
leeskies <lee2dskies@gmail.com>
2023-06-29 01:12:00 +0300

    asdfas

--- src/components/common/Search/UserResult.jsx
+++ src/components/common/Search/UserResult.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { UserHeader } from './UserHeader'
 import { ImageComp } from '../ImageComp'
 
 export const UserResult = ({ result }) => {
 
   const navigate = useNavigate()
-
+console.log(result);
   return (
     <button onClick={() => navigate('/user/' + result._id)} className='w-full p-2 hover:bg-sky-200 duration-200'>
-      <ImageComp w={'100px'} url={result.image.secure_url.replace('upload/', 'upload/c_fill,h_200,w_200/')} />
-      <footer></footer>
+      {/* <ImageComp w={'100px'} url={result.image.secure_url.replace('upload/', 'upload/c_fill,h_200,w_200/')} />
+      <footer></footer> */}
     </button>
   )
 }
