24d7392c88eddebda2f0b88e753c019bac40bbc9
leeskies <lee2dskies@gmail.com>
2023-06-28 12:05:30 +0300

    yo

--- src/components/protected/SideMenu.jsx
+++ src/components/protected/SideMenu.jsx
@@ -21,10 +21,6 @@ export const SideMenu = ({ }) => {
         {icon: <HandThumbUpIcon className='w-6 h-6' />, name: 'Liked', to: '/liked'},
         {icon: <UserIcon className='w-6 h-6' />, name: 'Profile', to: `/user/${user._id}`},
         {icon: <Cog6ToothIcon className='w-6 h-6' />, name: 'Settings', to: '/settings'},
-        {icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
-        <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
-        <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z"/>
-      </svg>}
     ]
 
   useEffect(() => {
@@ -44,10 +40,16 @@ export const SideMenu = ({ }) => {
         </button>
         <div className={`fixed flex items-center z-20 top-0 right-0 duration-300 ${open ? '' : 'translate-x-full'}`} >
             <button className='p-2 rounded-full text-black' onClick={() => setOpen(prev => !prev)}><ArrowRightIcon className='w-5 h-5'/></button>
-            <section className={`p-3 rounded-bl bg-neutral-950`}>
+            <section className={`p-3 rounded-bl flex flex-col items-center bg-neutral-950`}>
                 {links.map((link, i) => (
                     <NavLink to={link.to} className='p-2'>{link.icon}</NavLink>
                 ))}
+                <figure className='text-red-600'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
+                      <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+                      <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z"/>
+                    </svg>
+                </figure>
             </section>
         </div>
     </aside>
