e68c7bacad8586c92b4cf69a79dd7f3ee9b2044a
leeskies <lee2dskies@gmail.com>
2023-06-28 22:25:21 +0300

    x

--- src/components/common/ImageComp.jsx
+++ src/components/common/ImageComp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const ImageComp = ({ url, w = '100%', h = '100%' }) => {
   return (
     <figure style={{ width: w, height: h }}
-    className='flex justify-center items-center overflow-clip'>
+    className='flex justify-center items-center overflow-clip rounded'>
       <img src={url} className='min-w-full min-h-full object-cover' />
     </figure>
   )
--- src/components/common/Profile/Search/ResultsDisplay.jsx
+++ src/components/common/Profile/Search/ResultsDisplay.jsx
@@ -3,7 +3,7 @@ import { TextResult } from './TextResult'
 
 export const ResultsDisplay = ({ results }) => {
   return (
-    <section className='absolute bot-0 w-full z-20 bg-slate-100 text-slate-900'>
+    <section className='absolute bot-0 w-full z-20 bg-gradient-to-r from-sky-200 to-neutral-900 text-slate-900'>
         {results.map((result, i) => <TextResult result={result} />)}
     </section>
   )
