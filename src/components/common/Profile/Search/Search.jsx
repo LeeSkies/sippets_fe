38a2be2be97b05f3120ef0b5ae418a2dff62f643
leeskies <lee2dskies@gmail.com>
2023-06-28 21:56:35 +0300

    x

--- src/components/common/Profile/Search/Search.jsx
+++ src/components/common/Profile/Search/Search.jsx
@@ -7,8 +7,8 @@ export const Search = () => {
     const [loading, setLoading] = useState(true)
   return (
     <div className='w-full relative py-2 border-t border-t-slate-600'>
-        <SearchBar results={results} />
-        <ResultsDisplay results={results} />
+        <SearchBar results={results} setResults={setResults} />
+        <ResultsDisplay results={results} setResults={setResults} />
     </div>
   )
 }
--- src/components/common/Profile/Search/SearchBar.jsx
+++ src/components/common/Profile/Search/SearchBar.jsx
@@ -1,20 +1,31 @@
 import { ArrowPathRoundedSquareIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import React, { useRef, useState } from 'react'
+import instance from '../../../../services/axios'
+import { debounce } from 'lodash'
 
-export const SearchBar = () => {
+export const SearchBar = ({ setResults }) => {
     const inputRef = useRef()
     const [param, setParam] = useState('text')
 
-    const handleChange = () => {
+    const handleClick = () => {
         setParam(prev => prev == 'text' ? 'user' : 'text')
     }
+
+    const debouncedOnChange = debounce(async () => {
+        const val = inputRef.current.value;
+        if (val == '') return
+        const { data } = await instance.get(`/public/search/${param}`);
+        setResults(data)
+        console.log(data);
+      }, 3000);
+
   return (
     <section className='w-full flex items-center overflow-clip bg-gradient-to-r from-sky-200 to-transparent pl-1'>
-        <button onClick={handleChange} className='w-[100px] active:scale-95 duration-300 rounded-full p-[2px] px-2 space-x-1 border bg-slate-700 flex text-slate-300 items-center'>
+        <button onClick={handleClick} className='w-[100px] active:scale-95 duration-300 rounded-full p-[2px] px-2 space-x-1 border bg-slate-700 flex text-slate-300 items-center'>
             <p className='pb-[2px] grow'>{param}</p>
             <ArrowPathRoundedSquareIcon className='w-[12px] h-[12px]' />
         </button>
-        <input ref={inputRef} autoFocus={true} type="text" className='p-2 grow bg-transparent focus:outline-none caret-neutral-900 text-neutral-900' />
+        <input ref={inputRef} onChange={debouncedOnChange} autoFocus={true} type="text" className='p-2 grow bg-transparent focus:outline-none caret-neutral-900 text-neutral-900' />
         <button className=''>
             <MagnifyingGlassIcon className='w-6 h-6 text-sky-400 pr-2' />
         </button>
