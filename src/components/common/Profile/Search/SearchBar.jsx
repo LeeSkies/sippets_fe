4400af3a8f30f175b67197bfc5a5b6e2b48ceef2
leeskies <lee2dskies@gmail.com>
2023-06-28 15:08:42 +0300

    yo

--- src/components/common/Profile/Search/SearchBar.jsx
+++ src/components/common/Profile/Search/SearchBar.jsx
@@ -4,11 +4,11 @@ import React, { useRef } from 'react'
 export const SearchBar = () => {
     const inputRef = useRef()
   return (
-    <section className='w-full rounded-full flex overflow-clip'>
-        <input ref={inputRef} type="text" className='p-2 grow' />
+    <section className='w-full rounded-full flex overflow-clip bg-gradient-to-r from-sky-300 to-slate-300'>
+        <input ref={inputRef} type="text" className='p-2 grow bg-transparent' />
         <button className=''>
             <MagnifyingGlassIcon className='w-6 h-6' />
-            </button>
+        </button>
     </section>
   )
 }
