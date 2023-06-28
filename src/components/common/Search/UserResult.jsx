ab1ea5f5c09cc64defd5688e440293ca134e48a5
leeskies <lee2dskies@gmail.com>
2023-06-29 01:07:59 +0300

    redeploy

--- src/components/common/Search/UserResult.jsx
+++ src/components/common/Search/UserResult.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { UserHeader } from './UserHeader'
+import { ImageComp } from '../ImageComp'
 
 export const UserResult = ({ result }) => {
 
@@ -8,7 +9,7 @@ export const UserResult = ({ result }) => {
 
   return (
     <button onClick={() => navigate('/user/' + result._id)} className='w-full p-2 hover:bg-sky-200 duration-200'>
-      <UserHeader result={result} />
+      <ImageComp url={result.image.secure_url.replace('upload/', 'upload/c_fill,h_200,w_200/')} />
       <footer></footer>
     </button>
   )
