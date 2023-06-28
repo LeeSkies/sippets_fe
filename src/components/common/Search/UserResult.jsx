c0dd9997f02f7d2a9dfe099efb2f6581fbaf596b
leeskies <lee2dskies@gmail.com>
2023-06-29 01:23:21 +0300

    yo

--- src/components/common/Search/UserResult.jsx
+++ src/components/common/Search/UserResult.jsx
@@ -8,7 +8,7 @@ export const UserResult = ({ result }) => {
 console.log(result);
   return (
     <button onClick={() => navigate('/user/' + result._id)} className='w-full p-2 hover:bg-sky-200 duration-200'>
-      {/* <ImageComp w={'100px'} url={result.image.secure_url.replace('upload/', 'upload/c_fill,h_200,w_200/')} />
+      {/* <ImageComp w={'100px'} url={result.image.scure_url.replace('upload/', 'upload/c_fill,h_200,w_200/')} />
       <footer></footer> */}
     </button>
   )
