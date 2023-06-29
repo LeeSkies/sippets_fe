611d9876c97e30b6e5b82f91134a63e8e0660e80
leeskies <lee2dskies@gmail.com>
2023-06-29 04:00:54 +0300

    x

--- src/components/common/Search/UserResult.jsx
+++ src/components/common/Search/UserResult.jsx
@@ -9,7 +9,7 @@ console.log(result);
   return (
     <button onClick={() => navigate('/user/' + result._id)} className='w-full p-2 hover:bg-sky-200 duration-200'>
       {result?.image?.secure_url ? <ImageComp w={'100px'} url={result.image.secure_url.replace('upload/', 'upload/c_fill,h_200,w_200/')} />
-      : <p className='rounded w-[100px] flex items-center justify-center h-[100px] bg-blue-600'>{result.author.username.charAt(0)}</p>}
+      : <p className='rounded w-[100px] flex items-center justify-center h-[100px] bg-blue-600'>{result.username.charAt(0)}</p>}
       <footer></footer>
     </button>
   )
