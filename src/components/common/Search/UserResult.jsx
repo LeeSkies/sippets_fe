7db5480332fbd142b9f4d57e45dc0a2c75571bd1
leeskies <lee2dskies@gmail.com>
2023-06-29 17:55:16 +0300

    x

--- src/components/common/Search/UserResult.jsx
+++ src/components/common/Search/UserResult.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ImageComp } from '../ImageComp'
+import { UserIcon, UsersIcon } from '@heroicons/react/24/outline'
 
 export const UserResult = ({ result }) => {
 
@@ -13,14 +14,19 @@ export const UserResult = ({ result }) => {
   return (
     <button onClick={() => navigate('/user/' + result._id)} className='w-full flex h-[150px] pb-2 hover:bg-sky-200 duration-200'>
       {result?.image?.secure_url ? <ImageComp h={'100%'} rounded={false} url={result.image.secure_url.replace('upload/', 'upload/c_fill,h_200,w_200/')} />
-      : <p className='rounded flex items-center justify-center h-full aspect-square bg-blue-600'>{result.username.charAt(0)}</p>}
-      <section className='h-full w-full items-end flex flex-col justify-between p-2'>
+      : <p className='flex items-center justify-center w-[150px] aspect-square bg-blue-600'>{result.username.charAt(0)}</p>}
+      <section className='h-full w-full items-start text-start flex flex-col justify-between p-2'>
         <header className='text-lg font-bold'>{result.username}</header>
         <p>{trimStr()}</p>
         <footer className='w-full flex justify-around'>
-          <div className='flex space-x-1'></div>
-          <div className='flex space-x-1'></div>
-          <div className='flex space-x-1'></div>
+          <div className='flex space-x-1 text-black'>
+            <p>{result.followersCount}</p>
+            <UserIcon className='w-5 h-5' />
+          </div>
+          <div className='flex space-x-1 text-black'>
+            <p>{result.followingCount}</p>
+            <UsersIcon className='w-5 h-5' />
+          </div>
         </footer>
       </section>
     </button>
