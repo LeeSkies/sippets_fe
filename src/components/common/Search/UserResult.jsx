0e70b664693648fd8c54115cbb1b4eec62f65e1d
leeskies <lee2dskies@gmail.com>
2023-06-29 17:58:46 +0300

    x

--- src/components/common/Search/UserResult.jsx
+++ src/components/common/Search/UserResult.jsx
@@ -7,18 +7,13 @@ export const UserResult = ({ result }) => {
 
   const navigate = useNavigate()
 
-  const trimStr = (maxLen = 70) => {
-    return result?.bio.length > maxLen ? result.bio.substring(0, maxLen) + '...' : result.bio
-  }
-
   return (
-    <button onClick={() => navigate('/user/' + result._id)} className='w-full flex h-[150px] pb-2 hover:bg-sky-200 duration-200'>
-      {result?.image?.secure_url ? <ImageComp h={'100%'} rounded={false} url={result.image.secure_url.replace('upload/', 'upload/c_fill,h_200,w_200/')} />
-      : <p className='flex items-center justify-center w-[150px] aspect-square bg-blue-600'>{result.username.charAt(0)}</p>}
+    <button onClick={() => navigate('/user/' + result._id)} className='w-full flex pb-2 hover:bg-sky-200 duration-200'>
+      {result?.image?.secure_url ? <ImageComp h={'100px'} w={'100px'} rounded={false} url={result.image.secure_url.replace('upload/', 'upload/c_fill,h_200,w_200/')} />
+      : <p className='flex items-center justify-center w-[100px] aspect-square bg-blue-600'>{result.username.charAt(0)}</p>}
       <section className='h-full w-full items-start text-start flex flex-col justify-between p-2'>
         <header className='text-lg font-bold'>{result.username}</header>
-        <p>{trimStr()}</p>
-        <footer className='w-full flex justify-around'>
+        <footer className='flex space-x-3'>
           <div className='flex space-x-1 text-black'>
             <p>{result.followersCount}</p>
             <UserIcon className='w-5 h-5' />
