fbc29a7bc61ff7e5e9d44dd675100e3403fb0a89
leeskies <lee2dskies@gmail.com>
2023-06-29 15:57:16 +0300

    x

--- src/components/common/Search/UserResult.jsx
+++ src/components/common/Search/UserResult.jsx
@@ -7,7 +7,7 @@ export const UserResult = ({ result }) => {
   const navigate = useNavigate()
 console.log(result);
   return (
-    <button onClick={() => navigate('/user/' + result._id)} className='w-full p-2 hover:bg-sky-200 duration-200'>
+    result && <button onClick={() => navigate('/user/' + result._id)} className='w-full p-2 hover:bg-sky-200 duration-200'>
       {result?.image?.secure_url ? <ImageComp w={'100px'} url={result.image.secure_url.replace('upload/', 'upload/c_fill,h_200,w_200/')} />
       : <p className='rounded w-[100px] flex items-center justify-center h-[100px] bg-blue-600'>{result.username.charAt(0)}</p>}
       <footer></footer>
